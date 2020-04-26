4a7c68620a1112c40bcccc9cc4b9346cc3c7a0d1
capuriko <51282018+capuriko@users.noreply.github.com>
2020-04-26 20:33:38 +0900

    Create uk_stations.js

---
+++ js-tutorial/uk_stations.js
@@ -0,0 +1,18 @@
+const list = document.querySelector('.output ul');
+list.innerHTML = '';
+let stations = ['MAN675847583748sjt567654;Manchester Piccadilly',
+                'GNF576746573fhdg4737dh4;Greenfield',
+                'LIV5hg65hd737456236dch46dg4;Liverpool Lime Street',
+                'SYB4f65hf75f736463;Stalybridge',
+                'HUD5767ghtyfyr4536dh45dg45dg3;Huddersfield'];
+
+for (let i = 0; i < stations.length; i++) {
+  let input = stations[i];
+  let code = input.slice(0,3);
+  let semiC = input.indexOf(';');
+  let name = input.slice(semiC + 1);
+  let result = code + ': ' + name;
+  let listItem = document.createElement('li');
+  listItem.textContent = result;
+  list.appendChild(listItem);
+}
