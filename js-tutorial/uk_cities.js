b134f5244664433272a5cc3854f91d858f47c928
capuriko <51282018+capuriko@users.noreply.github.com>
2020-04-26 20:31:38 +0900

    Create uk_cities.js

---
+++ js-tutorial/uk_cities.js
@@ -0,0 +1,15 @@
+const list = document.querySelector('.output ul');
+list.innerHTML = '';
+let cities = ['lonDon', 'ManCHESTer', 'BiRmiNGHAM', 'liVERpoOL'];
+
+for (let i = 0; i < cities.length; i++) {
+  let input = cities[i];
+  let lower = input.toLowerCase();
+  let firstLetter = lower.slice(0,1);
+  let capitalized = lower.replace(firstLetter,firstLetter.toUpperCase());
+  let result = capitalized;
+  let listItem = document.createElement('li');
+  listItem.textContent = result;
+  list.appendChild(listItem);
+
+}
